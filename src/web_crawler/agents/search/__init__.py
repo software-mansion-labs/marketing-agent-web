2eada1d3c4defc3b962fd83260990eaa7e5d76cf
Piotr Zborowski <zborowsk@student.agh.edu.pl>
2026-02-09 10:40:34 +0100

    /search.__init__ fix

--- src/web_crawler/agents/search/__init__.py
+++ src/web_crawler/agents/search/__init__.py
@@ -1,4 +1,4 @@
 from web_crawler.agents.search.node import SearchAgentNode
 from web_crawler.agents.search.state import SearchAgentState
 
-__all__ = [SearchAgentNode, SearchAgentState]
+__all__ = ["SearchAgentNode", "SearchAgentState"]
